6b65961e2e08c5090d93c15240a1ec411a82d4d5
CoolAid48 <atgunsel@gmail.com>
2025-10-16 20:26:42 -0400

    Update to 1.21.9-1.21.10
    - Keybinds category changed from simple string to "KeyBinding.Category" object via identifiers since you can’t pass a plain string anymore.
    - Other Small tweaks
    - NeoForge Support Coming Soon™

--- src/main/java/coolaid/disablesigngui/mixin/MixinClientPlayerEntity.java
+++ src/main/java/coolaid/disablesigngui/mixin/MixinClientPlayerEntity.java
@@ -1,8 +1,9 @@
 package coolaid.disablesigngui.mixin;
 
-import coolaid.disablesigngui.DisableSigngui;
-import net.minecraft.client.network.ClientPlayerEntity;
+import coolaid.disablesigngui.DisableSignGUI;
+
 import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayerEntity.class)
 public abstract class MixinClientPlayerEntity {
 
-	@Inject(method = "openEditSignScreen(Lnet/minecraft/block/entity/SignBlockEntity;Z)V", at = @At("HEAD"), cancellable = true)
-	private void onOpenEditSignScreen(SignBlockEntity sign, boolean front, CallbackInfo info) {
-		if (!DisableSigngui.isSignGuiEnabled) {
-			info.cancel();
-		}
-	}
+    @Inject(method = "openEditSignScreen(Lnet/minecraft/block/entity/SignBlockEntity;Z)V", at = @At("HEAD"), cancellable = true)
+    private void onOpenEditSignScreen(SignBlockEntity sign, boolean front, CallbackInfo info) {
+        if (!DisableSignGUI.isSignGuiEnabled) {
+            info.cancel();
+        }
+    }
 }
